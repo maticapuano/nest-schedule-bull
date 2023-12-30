33467b8356c16b86cff42e879b3a889b23cfaa69
Matias Capuano <maticapuano97@gmail.com>
2023-12-30 00:26:03 -0300

    chore: register redis config

--- src/schedule.module.ts
+++ src/schedule.module.ts
@@ -1,4 +1,20 @@
-import { Module } from "@nestjs/common";
+import { DynamicModule, Module } from "@nestjs/common";
+import { ConnectionOptions } from "bullmq";
+import { CRON_MODULE_REDIS_OPTIONS } from "./constants/metadata";
 
 @Module({})
-export class ScheduleBullModule {}
+export class ScheduleBullModule {
+  public static forRoot(options: ConnectionOptions): DynamicModule {
+    return {
+      global: true,
+      module: ScheduleBullModule,
+      providers: [
+        {
+          provide: CRON_MODULE_REDIS_OPTIONS,
+          useValue: options ?? {},
+        },
+      ],
+      exports: [],
+    };
+  }
+}
